1756ae2a195af750616f0ce1b6b31f40572ccc47
surovtsev_vrn <alexandr.surovtsev.vrn@yandex.ru>
2021-03-19 21:08:12 +0300

    add main activity

---
+++ app/src/main/java/com/surovtsev/cool_3d_minesweeper/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.surovtsev.cool_3d_minesweeper.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.surovtsev.cool_3d_minesweeper.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
