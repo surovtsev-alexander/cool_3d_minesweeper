1dfe23371844475d780f21a0fc30f314d18cfed4
surovtsev_vrn <alexandr.surovtsev.vrn@yandex.ru>
2021-03-19 21:16:24 +0300

    add game activity

---
+++ app/src/main/java/com/surovtsev/cool_3d_minesweeper/activities/GameActivity.kt
@@ -0,0 +1,14 @@
+package com.surovtsev.cool_3d_minesweeper.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.surovtsev.cool_3d_minesweeper.R
+
+class GameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+
+
+    }
+}
--- app/src/main/java/com/surovtsev/cool_3d_minesweeper/activities/MainActivity.kt
+++ app/src/main/java/com/surovtsev/cool_3d_minesweeper/activities/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.surovtsev.cool_3d_minesweeper.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.surovtsev.cool_3d_minesweeper.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_start_game.setOnClickListener {
+            startActivity(Intent(this, GameActivity::class.java))
+        }
     }
 }
