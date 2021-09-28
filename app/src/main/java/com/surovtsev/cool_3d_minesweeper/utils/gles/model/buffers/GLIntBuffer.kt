72d90e0734664d5244d488bd30686cbfa5c55a61
surovtsev_vrn <alexandr.surovtsev.vrn@yandex.ru>
2021-09-28 23:15:20 +0300

    move comments

--- app/src/main/java/com/surovtsev/cool_3d_minesweeper/utils/gles/model/buffers/GLIntBuffer.kt
+++ app/src/main/java/com/surovtsev/cool_3d_minesweeper/utils/gles/model/buffers/GLIntBuffer.kt
@@ -12,37 +12,40 @@ import java.nio.IntBuffer
 /* TODO: move to Dagger */
 @Suppress("unused")
 class GLIntBuffer (data: IntArray) {
-// Bind to the buffer.
-
-// Transfer data to native memory.
-
-// Transfer data from native memory to the GPU buffer.
-
-// IMPORTANT: Unbind from the buffer when we're done with it.
-
     val bufferId: Int
     val dataBuffer: IntBuffer
 
     init {
         val buffers = intArrayOf(0)
+
         GLES20.glGenBuffers(buffers.count(), buffers, 0)
+
         if (buffers[0] == 0) {
             throw RuntimeException(
-                "Could not create a new vertex buffer object.")
+                "Could not create a new vertex buffer object."
+            )
         }
         bufferId = buffers[0]
+
+        // Bind to the buffer.
         GLES20.glBindBuffer(GLES20.GL_ELEMENT_ARRAY_BUFFER, bufferId)
+
+        // Transfer data to native memory.
         dataBuffer = ByteBuffer
-            .allocateDirect(data.count() * Constants.BytesPerInt)
+            .allocateDirect(data.count() * Constants.BytesPerFloat)
             .order(ByteOrder.nativeOrder())
             .asIntBuffer()
             .put(data)
         dataBuffer.position(0)
+
+        // Transfer data from native memory to the GPU buffer.
         GLES20.glBufferData(
             GLES20.GL_ARRAY_BUFFER,
             dataBuffer.capacity() * Constants.BytesPerInt,
             dataBuffer, GLES20.GL_STATIC_DRAW
         )
+
+        // IMPORTANT: Unbind from the buffer when we're done with it.
         GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, 0)
     }
 
