140b509588b43159ba45dcd4a3d918af0740aa45
surovtsev_vrn <alexandr.surovtsev.vrn@yandex.ru>
2021-12-26 08:58:51 +0300

    remove useless code

--- sources/logic/core/src/main/java/com/surovtsev/core/settings/SettingsListData.kt
+++
@@ -1,7 +0,0 @@
-package com.surovtsev.core.settings
-
-import com.surovtsev.core.dataconstructions.MyLiveData
-import com.surovtsev.core.room.dao.SettingsList
-
-typealias SettingsListData = MyLiveData<SettingsList>
-
