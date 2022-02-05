244b53b4c2e75077ca57a5348c6e7fb88fdc8f58
surovtsev_vrn <alexandr.surovtsev.vrn@yandex.ru>
2022-02-05 21:15:34 +0300

    [refactoring] settings.gradle.kts: add project dir dsl

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,97 +1,117 @@
 rootProject.name = "cool_3d_minesweeper"
 
 
-/// region [values]: modules descriptions
-val appModule = ":app"
-
-val appModuleDir = "sources"
-
-
-val coreModule = ":core"
-val finiteStateMachineModule = ":finitestatemachine"
-val gameLogicModule = ":gamelogic"
-val gameStateModule = ":gamestate"
-val gameStateHolderModule = ":gamestateholder"
-val restartableCoroutineScopeModule = ":restartablecoroutinescope"
-val subscriptionsHolderModule = ":subscriptionsholder"
-val timeSpanModule = ":timespan"
-val touchListenerModule = ":touchlistener"
-val utilsModule = ":utils"
-
-val commonLogicModules = arrayOf(
-    finiteStateMachineModule,
-    timeSpanModule,
-    touchListenerModule,
-    utilsModule,
-)
-val commonLogicDir = "sources/logic/common"
-
-val restartableCoroutineScopeModules = arrayOf(
-    restartableCoroutineScopeModule,
-    subscriptionsHolderModule,
-)
-val restartableCoroutineScopeDir = "sources/logic/common/restartablecoroutinescope"
-
-val specificLogicModules = arrayOf(
-    coreModule,
-    gameLogicModule,
-    gameStateModule,
-    gameStateHolderModule,
-)
-val specificLogicDir = "sources/logic/specific"
-
-val gameScreenModule = ":gamescreen"
-val mainScreenModule = ":mainscreen"
-
-val screensModules = arrayOf(
-    gameScreenModule,
-    mainScreenModule,
-)
-val screensDir = "sources/screens"
+/// region [values]: constants (Modules and Folders)
+object Modules {
+    const val app = ":app"
+    const val core = ":core"
+    const val finiteStateMachine = ":finitestatemachine"
+    const val helpScreen = ":helpscreen"
+    const val gameLogic = ":gamelogic"
+    const val gameScreen = ":gamescreen"
+    const val gameState = ":gamestate"
+    const val gameStateHolder = ":gamestateholder"
+    const val mainScreen = ":mainscreen"
+    const val rankingScreen = ":rankingscreen"
+    const val restartableCoroutineScope = ":restartablecoroutinescope"
+    const val settingsScreen = ":settingsscreen"
+    const val subscriptionsHolder = ":subscriptionsholder"
+    const val timeSpan = ":timespan"
+    const val touchListener = ":touchlistener"
+    const val utils = ":utils"
+}
 
 
-val helpScreenModule = ":helpscreen"
-val rankingScreenModule = ":rankingscreen"
-val settingsScreenModule = ":settingsscreen"
-
-val optionalScreensModules = arrayOf(
-    helpScreenModule,
-    rankingScreenModule,
-    settingsScreenModule,
-)
-val optionalScreensDir = "sources/screens/optional"
+object Folders {
+    const val common = "common"
+    const val logic = "logic"
+    const val optional = "optional"
+    const val restartableCoroutineScope = "restartablecoroutinescope"
+    const val root = "sources"
+    const val screens = "screens"
+    const val specific = "specific"
+}
 /// endregion
 
 
-/// region [action]: including modules
-includeModule(
-    appModule,
-    appModuleDir,
-)
-includeModules(
-    commonLogicModules,
-    commonLogicDir,
-)
-includeModules(
-    restartableCoroutineScopeModules,
-    restartableCoroutineScopeDir,
-)
-includeModules(
-    specificLogicModules,
-    specificLogicDir,
-)
-includeModules(
-    screensModules,
-    screensDir,
-)
-includeModules(
-    optionalScreensModules,
-    optionalScreensDir,
-)
+/// region [action]: declare project modules tree
+projectDir(
+    Folders.root,
+    arrayOf(Modules.app),
+) {
+    projectDir(
+        subfolder(Folders.logic),
+    ) {
+        projectDir(
+           subfolder(Folders.common),
+           arrayOf(
+               Modules.finiteStateMachine,
+               Modules.timeSpan,
+               Modules.touchListener,
+               Modules.utils,
+           ),
+        ) {
+            projectDir(
+                subfolder(Folders.restartableCoroutineScope),
+                arrayOf(
+                    Modules.restartableCoroutineScope,
+                    Modules.subscriptionsHolder,
+                )
+            )
+        }
+
+        projectDir(
+            subfolder(Folders.specific),
+            arrayOf(
+                Modules.core,
+                Modules.gameLogic,
+                Modules.gameState,
+                Modules.gameStateHolder,
+            )
+        )
+    }
+
+    projectDir(
+        subfolder(Folders.screens),
+        arrayOf(
+            Modules.gameScreen,
+            Modules.mainScreen,
+        ),
+    ) {
+        projectDir(
+            subfolder(Folders.optional),
+            arrayOf(
+                Modules.helpScreen,
+                Modules.rankingScreen,
+                Modules.settingsScreen,
+            ),
+        )
+    }
+}
 /// endregion
 
 
-/// region [helper methods]: include modules
+/// region [helper methods]: include modules of project tree
+fun projectDir(
+    dir: String,
+    modules: Array<String> = emptyArray(),
+    subDirsCreator: (ParentDirHolder.() -> Unit)? = null,
+) {
+    includeModules(
+        modules,
+        dir
+    )
+    subDirsCreator?.invoke(ParentDirHolder(dir))
+}
+
+data class ParentDirHolder(
+    val parentDir: String,
+) {
+    fun subfolder(
+        dir: String
+    ) = "$parentDir/$dir"
+}
+
 fun includeModules(
     modules: Array<String>,
     dir: String
