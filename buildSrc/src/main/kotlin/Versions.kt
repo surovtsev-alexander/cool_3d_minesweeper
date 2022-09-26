961cff3b6ee41784e9790f0fe9bd978be0f23d6e
surovtsev-alexander <alexander.surovtsev.ya@yandex.ru>
2022-09-26 13:38:25 +0300

    update revision

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -13,7 +13,7 @@ object Versions {
     object Library {
         private const val major = 1 // change when you make incompatible API changes
         private const val minor = 5 // change when you add functionality in a backward-compatible manner
-        private const val build = 0 // change when you make backward-compatible bug fixes
+        private const val build = 1 // change when you make backward-compatible bug fixes
 
         const val versionCode = (major * 100 + minor) * 100 + build
         const val versionName = "$major.$minor.$build"
